5dec220bca5cb3555545e553f0a343f7602d1f4e
zwr <954489951@qq.com>
2019-04-02 21:19:11 +0800

    实验4

--- 4/memory_spy.cpp
+++ 4/memory_spy.cpp
@@ -2,32 +2,47 @@
 #include <iostream>
 #include <Shlwapi.h>
 #include <iomanip>
+#define DIV 1024
 #pragma comment(lib, "shlwapi.lib")
 using namespace std;
 
 int main()
 {
-	MEMORYSTATUSEX statex;	//
+	//来自https://docs.microsoft.com/zh-cn/windows/desktop/api/sysinfoapi/nf-sysinfoapi-globalmemorystatusex
+	MEMORYSTATUSEX statex;
 	statex.dwLength = sizeof(statex);
-	//获取系统内存信息
-	GlobalMemoryStatusEx(&statex);
-	printf("-----------------------内存信息-----------------------\n");
-	//内存使用率
-	printf("物理内存的使用率为:%ld%%\n", statex.dwMemoryLoad);
-	//物理内存
-	printf("物理内存的总容量为: %.2fGB.\n", (float)statex.ullTotalPhys / 1024 / 1024 / 1024);
-	//可用物理内存
-	printf("可用的物理内存为: %.2fGB.\n", (float)statex.ullAvailPhys / 1024 / 1024 / 1024);
-	//提交的内存限制
-	printf("总的交换文件为:%.2fGB.\n", (float)statex.ullTotalPageFile / 1024 / 1024 / 1024);
-	//当前进程可以提交的最大内存量
-	printf("可用的交换文件为：%.2fGB.\n", (float)statex.ullAvailPageFile / 1024 / 1024 / 1024);
-	//虚拟内存
-	printf("虚拟内存的总容量为：%.2fGB.\n", (float)statex.ullTotalVirtual/1024 / 1024 / 1024);
-	//可用虚拟内存
-	printf("可用的虚拟内存为：%.2fGB.\n", (float)statex.ullAvailVirtual/1024 / 1024 / 1024);
-	//保留字段
-	printf("保留字段的容量为：%.2fByte.\n",statex.ullAvailExtendedVirtual);
-	
+	//获取物理内存信息
+	if (GlobalMemoryStatusEx(&statex) == false)
+	{
+		cout << GetLastError() << endl;
+	}
+	cout << "-------------------statement of memory-------------------" << endl;
+	cout << "The statement of physical memory: " << endl;
+	//内存使用
+	cout << "There is " << statex.dwLength << " percent of memory in use." << endl;
+	//物理内存大小
+	cout << "There are " << statex.ullTotalPhys / DIV << " total KB of physical memory." << endl;
+	//空闲内存大小
+	cout << "There are " << statex.ullAvailPhys / DIV << " free KB of physical memory." << endl;
+	//总的页文件大小
+	cout << "There are " << statex.ullTotalPageFile / DIV << " total KB of paging file." << endl;
+	//空闲页文件大小
+	cout << "There are " << statex.ullAvailPageFile / DIV << " free KB of paging file." << endl;
+	//虚拟内存大小
+	cout << "There are " << statex.ullTotalVirtual / DIV << " total KB of virtual memory." << endl;
+	//可用的虚拟内存大小
+	cout << "There are " << statex.ullAvailVirtual / DIV << " free  KB of virtual memory." << endl;
+
+	//来自书本P286
+	//获得系统信息
+	SYSTEM_INFO si;
+	ZeroMemory(&si, sizeof(si));
+	GetSystemInfo(&si);
+
+	cout << "-------------------statement of system-------------------" << endl;
+	//虚拟内存页大小
+	cout << "Virtual memory page size: " << si.dwPageSize / DIV << "KB" << endl;
+	cout << "Minimum application address: " << si.lpMinimumApplicationAddress << endl;
+	cout << "Maximum application address: " << si.lpMaximumApplicationAddress << endl;
 	return 0;
 }
